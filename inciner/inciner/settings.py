e7cb3867f30956182d461dcc72621b80595438c9
GNAchoux <hi5373934@gmail.com>
2026-07-20 12:12:03 +0900

    chore: DB설정에서 sqlite 분기 제거 후 mysql로 고정

--- inciner/inciner/settings.py
+++ inciner/inciner/settings.py
@@ -82,16 +82,7 @@ WSGI_APPLICATION = 'inciner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DB_ENGINE = config('DB_ENGINE', default='mysql')
-if DB_ENGINE == 'sqlite':
-    DATABASES = {
-        'default' : {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3'
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': config('DB_NAME'),
